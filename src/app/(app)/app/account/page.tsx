429a332decf8343ca6aba840aa73e2e4ff618501
khalilos34 <khalil.naoueji@gmail.com>
2024-04-16 22:35:44 +0200

    account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,17 @@
+import ContentBlock from "@/components/ContentBlock";
+
 const AccountPage = () => {
-  return <div>AccountPage</div>;
+  return (
+    <main>
+      <h1 className="my-8 text-2xl font-medium  leading-6 text-white">
+        Your Account
+      </h1>
+
+      <ContentBlock className="flex h-[500px] items-center justify-center">
+        <p>Welcome Amoula ta7founa .</p>
+      </ContentBlock>
+    </main>
+  );
 };
 
 export default AccountPage;
--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -1,6 +1,17 @@
-const ContentBlock = ({ children }: { children: React.ReactNode }) => {
+import { cn } from "@/lib/utils";
+type ContentBlockProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const ContentBlock = ({ children, className }: ContentBlockProps) => {
   return (
-    <div className="size-full overflow-hidden rounded-md bg-[#f7f8fa] shadow-sm">
+    <div
+      className={cn(
+        "size-full overflow-hidden rounded-md bg-[#f7f8fa] shadow-sm",
+        className,
+      )}
+    >
       {children}
     </div>
   );
